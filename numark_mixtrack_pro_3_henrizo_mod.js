e5d1881fc418cf55564aa77680f0ea66f4c3804f
Matheus <matheus.hs97@gmail.com>
2019-12-19 18:13:02 -0300

    Corrected the jumping beat causing pitch alteration bug.

--- numark_mixtrack_pro_3_henrizo_mod.js
+++ numark_mixtrack_pro_3_henrizo_mod.js
@@ -49,6 +49,72 @@ var beta = alpha / 32;  // Adjust to suit.
  **************************/
 var loopsize = [2, 4, 8, 16, 0.125, 0.25, 0.5, 1];
 
+/************************  GPL v2 licence  *****************************
+ * Numark Mixtrack Pro 3 controller script (Henrizo mod)
+ * Author: Stéphane Morin
+ * Modified by: Radu Suciu, Henrizo
+ *
+ * Key features
+ * ------------
+ * - ICUT effect for scratching
+ * - Fader Start
+ * - press/double press/long press handling
+ * - Smart PFL
+ * - 4 deck support
+ * - Full effect chains support from Deere Skin
+ **********************************************************************
+ * User References
+ * ---------------
+ * Wiki/manual : http://mixxx.org/wiki/doku.php/numark_mixtrack_pro_3
+ * Scripting manual: https://www.mixxx.org/wiki/doku.php/midi_scripting
+ *                   https://www.mixxx.org/wiki/doku.php/mixxxcontrols 
+ * e-mail : steph@smorin.com, matheus.hs97@gmail.com
+ *
+ * Thanks
+ * ----------------
+ * Thanks to Chloé AVRILLON (DJ Chloé) and authors of other scripts and particularly 
+ * to authors of Numark Dj2Go, KANE QuNeo, Vestax-VCI-400
+ *
+ * Revision history
+ * ----------------
+ * 2016-01-12 (V0.9) to 2016-01-15 (1.0 beta 3) - Chloé AVRILLON 
+ * 2016-02-17 (1.0 beta 4) 2016-04-08 (V1.3 )- Stéphane Morin
+ * 2016-04-08 (1.3) - Stéphane Morin - https://github.com/mixxxdj/mixxx/pull/905
+ * 2016-09-14 (1.31) - Stefan Mikolajczyk - https://github.com/mixxxdj/mixxx/pull/1012
+ * 2016-04-08 (1.4) to 2017-01-05 (2.2) - Stéphane Morin - https://github.com/mixxxdj/mixxx/pull/1014
+ * 2017-02-10 (2.3) - Radu Suciu - https://github.com/mixxxdj/mixxx/pull/1180
+ * 2018-01-20 (2.4) - NTMusic - Shift+Filter can control either FX4 or channel gain
+ * 
+ ** Henrizo mods (https://github.com/henrizo-configs/mixxx) :
+ ** --------------------------------------------------------
+ ** I decided to mod this config file so the controller adjusts to my needs and style. Of course, 
+ ** I'm standing in the shoulders of giants here. Thanks again to all the folks above.
+ *
+ * 2019-12-17 (V0) - Shift + reloop_exit : activates beatloop
+                   - Effects are focused with TAP + FX_ and activated with FX, unlike the original file, 
+                     which inverts these controls.
+                   - The top corners pitch bend buttons sets the pitch (independent of the tempo) up or down, 
+                   - and pressing Shift with these buttons resets the pitch.
+ ***********************************************************************
+ *                           GPL v2 licence
+ *                           -------------- 
+ * Numark Mixtrack Pro 3 controller script 2.4 for Mixxx 2.1+
+ * Copyright (C) 2016 Stéphane Morin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ ***********************************************************************/
 ////////////////////////////////////////////////////////////////////////
 // JSHint configuration                                               //
 ////////////////////////////////////////////////////////////////////////
@@ -59,6 +125,7 @@ var loopsize = [2, 4, 8, 16, 0.125, 0.25, 0.5, 1];
 /* jshint sub:true                                                    */
 /* jshint shadow:true                                                 */
 ////////////////////////////////////////////////////////////////////////
+
 
 var NumarkMixtrack3 = {
     group: '[Master]',
@@ -1679,8 +1746,9 @@ NumarkMixtrack3.PitchBendMinusButton = function(channel, control, value, status,
         else if (deck.TapDown) {
             engine.setValue(deck.group, "beatjump_backward", true);
         }        
-    } else if (!deck.shiftKey) {
-        engine.setValue(deck.group, "pitch_down", true);
+        else {
+            engine.setValue(deck.group, "pitch_down", true);
+        }
     }
 };
 
@@ -1694,8 +1762,9 @@ NumarkMixtrack3.PitchBendPlusButton = function(channel, control, value, status, 
         else if (deck.TapDown) {
             engine.setValue(deck.group, "beatjump_forward", true);
         }
-    } else if (!deck.shiftKey) {
-        engine.setValue(deck.group, "pitch_up", true);
+        else {
+            engine.setValue(deck.group, "pitch_up", true);
+        }
     }
 };
 
